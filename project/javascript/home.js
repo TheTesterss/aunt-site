5b9dbc6a9f89558781f03eb778b43f4b43c5ddf5
TheTesterss <morganjaouen65636@gmail.com>
2024-07-20 14:31:40 +0200

    V1 few bases

---
+++ project/javascript/home.js
@@ -0,0 +1,31 @@
+let hamburger = document.querySelector(".hamburger");
+let header = document.querySelector(".header");
+let a = document.querySelectorAll(".navigation_link")
+let root = document.documentElement;
+let navbg = document.querySelector(".nav-burger");
+let box = document.querySelectorAll(".box");
+
+
+
+hamburger.addEventListener("click", () => {
+    if (hamburger.classList.contains("enabled")) {
+        hamburger.classList.remove("enabled");
+        hamburger.classList.add("disabled");
+        header.classList.remove("activated");
+        navbg.classList.remove("opened")
+        for(ab of a) {
+            ab.classList.remove("acti");
+            ab.classList.add("a");
+        }
+
+    } else {
+        hamburger.classList.add("enabled");
+        hamburger.classList.remove("disabled");
+        header.classList.add("activated");
+        navbg.classList.add("opened")
+        for(ab of a) {
+            ab.classList.add("acti");
+            ab.classList.remove("a");
+        }
+    };
+});
